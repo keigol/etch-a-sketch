a3d0fc0b840884364d9a7364639d1d5f67a3df87
Keigo Lee <keigo-l@Live.com>
2022-05-15 11:09:28 -0700

    update color style

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var length = 16;
+const length = 100;
 
 function run() {
     setupCanvas();
 }
 
-
+function draw(e) {
+    e.target.style.background = 'rgb(78, 78, 78)';
+    console.log('hi');
+}
 
 function setupCanvas() {
     const canvas = document.querySelector('.canvas');
@@ -12,6 +15,7 @@ function setupCanvas() {
     for (let i = 0; i < length*length; i++) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.addEventListener('mouseenter', draw);
         canvas.appendChild(pixel);
     }
 }
